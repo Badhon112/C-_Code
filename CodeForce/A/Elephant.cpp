6933ea0c8196307c2716e7e5cbf116b531a5c7bf
Badhon Biswas <biswasbadhon896@gmail.com>
2021-10-21 20:04:29 -0800

    21-10-2021

--- CodeForce/A/Elephant.cpp
+++ CodeForce/A/Elephant.cpp
@@ -2,4 +2,15 @@
 using namespace std;
 int main()
 {
+    int x, n;
+    cin >> n;
+    x = n / 5;
+    if (n % 5 == 0)
+    {
+        cout << x << endl;
+    }
+    else
+    {
+        cout << x + 1 << endl;
+    }
 }
